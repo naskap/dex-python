c5a22a0032cc91200b9eb0bcbd0b398b36752c00
Nathan Kaplan <nathank_18@yahoo.com>
2025-04-13 18:12:39 -0400

    Simplification pass and add helper traversers

--- src/simplify.py
+++ src/simplify.py
@@ -1,61 +1,49 @@
 from AST import *
 from typecheck import isValue
 from typeinfer import TypeInfer
+from traversers import ExprMutator, ExprVisitor
+from typing import Iterable
 
+var_num = 1
+
+def get_fresh_var():
+    toreturn = Var("fvar{}".format(var_num))
+    var_num = var_num + 1
+    return toreturn
 
 
 def subst(e : 'Expr', x : Var, v : Value) -> 'Expr':
-    if(e == x):
-        return v
-    elif(isinstance(e, Var)):
-        return e
-    elif(isinstance(e, Application)):
-        return Application(subst(e.func, x, v), subst(e.arg))
-    elif(isinstance(e,Function)):
-        if(e.var == x):
+    class SubstMutator(ExprMutator):
+        def __init__(self, x: Var, v: Value):
+            self.x = x
+            self.v = v
+        
+        def mutate_var(self, e: Var) -> 'Expr':
+            if e == self.x:
+                return self.v
             return e
-        else:
-            return Function(e.var, subst(e.body, x, v), subst(e.param_type,x,v))
-    elif(isinstance(e,Fin)):
-        return Fin(subst(e.end, x, v))
-    elif(isinstance(e,View)):
-        # Substitutes propogate through a view expression
-        return View(subst(e.var, x, v), subst(e.body, x, v), subst(e.var_type, x, v))
-    elif(isinstance(e,Pair)):
-        return Pair(subst(e.left, x, v), subst(e.right, x, v))
-    elif(isinstance(e,Let)):
-        if(e.var == x):
-            return e
-        else:
-            return Let(e.var, subst(e.value, x, v), subst(e.body, x, v), subst(e.var_type,x,v))
-    elif(isinstance(e,Index)):
-        return Index(subst(e.array, x, v), subst(e.index, x, v))
-    elif(isinstance(e,For)):
-        if(e.var == x):
-            # Variable shadowing, don't substitute in the body
-            return e
-        else:
-            return For(e.var, subst(e.body, x, v), subst(e.var_type, x, v))
-    elif(isinstance(e,Fst)):
-        return Fst(subst(e.pair, x, v))
-    elif(isinstance(e,Snd)):
-        return Snd(subst(e.pair, x, v))
-    elif(isinstance(e,RefSlice)):
-        return RefSlice(subst(e.ref, x, v), subst(e.index, x, v))
-    elif(isinstance(e,runAccum)):
-        return runAccum(subst(e.update_fun, x, v))
-    elif(isinstance(e,PlusEquals)):
-        return PlusEquals(subst(e.dest, x, v), subst(e.src, x, v))
-    elif(isinstance(e,Add)):
-        return Add(subst(e.left, x, v), subst(e.right, x, v))
-    elif(isinstance(e,Multiply)):
-        return Multiply(subst(e.left, x, v), subst(e.right, x, v))
-    elif(isinstance(e, ArrayType)):
-        return ArrayType(subst(e.index_set, x, v), subst(e.elmt_type, x, v))
-    elif(isinstance(e, FunctionType)):
-        return FunctionType(subst(e.tau1, x, v), subst(e.tau2, x, v), subst(e.effect, x, v))
-    else:
-        return e
+        
+        def mutate_function(self, e: Function) -> Function:
+            if e.var == self.x:
+                # Variable shadowing, don't substitute in the body
+                return e
+            else:
+                return Function(e.var, self.mutate(e.body), self.mutate(e.param_type))
+        
+        def mutate_let(self, e: Let) -> Let:
+            if e.var == self.x:
+                return e
+            else:
+                return Let(e.var, self.mutate(e.value), self.mutate(e.body), self.mutate(e.var_type))
+        
+        def mutate_for(self, e: For) -> For:
+            if e.var == self.x:
+                return e
+            else:
+                return For(e.var, self.mutate(e.body), self.mutate(e.var_type))
+    
+    substituter = SubstMutator(x, v)
+    return substituter.mutate(e)
 
 def composeContexts(E1 : Context, E2 : Context) -> Context:
     if(isinstance(E1, LetContext)):
@@ -73,17 +61,32 @@ def binders(E : Context) -> list[tuple[Var, DexType]]:
 
     return toreturn
 
-def bindingList(Ed : Context, v : Value) -> set[Var]:
+
+def freeVars(v : Value) -> set[Var]:
+    class FreeVarsVisitor(ExprVisitor):
+        def __init__(self):
+            self.free_vars = set()
+        
+        def visit_var(self, e):
+            self.free_vars.add(e)
+            return super().visit_var(e)
+        
+    visitor = FreeVarsVisitor()
+    visitor.visit(v)
+    return visitor.free_vars
+
+
+def bindingList(Ed : Context, v : Value, extraBinding : tuple[Var, DexType] = None) -> set[Var]:
     '''
     First approximation calculation of the free variables of v that are bound by Ed
     In the paper denoted binders(Ed) |- v |> result 
     '''
-    return bindingListR(binders(Ed), v)
 
-    
-def freeVars(v : Value) -> set[Var]:
-    pass
+    EdBinders = binders(Ed)
+    if(extraBinding is not None):
+        EdBinders.insert(0, extraBinding)
 
+    return bindingListR(EdBinders, v)
 
 
 def bindingListR(potentialBindings : list[tuple[Var, DexType]], v : Value) -> set[Var]:
@@ -96,7 +99,9 @@ def bindingListR(potentialBindings : list[tuple[Var, DexType]], v : Value) -> se
 
     freeVarsV = freeVars(v)
 
-    if(x1 in freeVarsV and len(freeVars(tau1).intersection(ybar)) == 0):
+    # Rule Used and NotUsed
+    if(x1 in freeVarsV):
+        assert len(freeVars(tau1).intersection(ybar)) == 0, " Not supported, see bottom of page 13 of Dex paper for explanation"
         ybar.add(x1)
         return ybar
     elif(x1 not in freeVarsV):
@@ -104,30 +109,77 @@ def bindingListR(potentialBindings : list[tuple[Var, DexType]], v : Value) -> se
 
     return []
 
+def applyContext(Ed : Context, v : Value) -> Expr:
+    if(isinstance(Ed, Hole)):
+        return v
+    elif(isinstance(Ed, LetContext)):
+        return Let(Ed.var, Ed.var_type, Ed.expr, applyContext(Ed.context, v))
+    
+    assert False, "Invalid context type {}".format(type(Ed))
 
-# def simplify(e : 'Expr') -> Tuple[Context, Value]:
+def createTuple(vars : Iterable[Var]) -> Pair:
+    assert len(vars) > 0, "Not handled"
+
+    var_iterator = iter(vars)
+    if(len(vars) == 1):
+        return next(var_iterator) # Note: doesn't follow type annotation
+    
+    cur_pair = Pair(next(var_iterator), next(var_iterator))
+    to_add = next(var_iterator, default = None)
+    while(to_add is not None):
+        cur_pair = Pair(to_add, cur_pair)
+        to_add = next(var_iterator, default = None)
+    
+    return cur_pair
+
+
+def simplify(e : 'Expr') -> Tuple[Context, Value]:
         
-#     if(isValue(e)):
-#         return Hole(), e
-#     elif(isinstance(e, Application)):
-#         subst_result = subst(e.func.body, e.func.var, e.arg)
-#         return simplify(subst_result)
-#     elif(isinstance(e, Let)):
-#         E1d, v1 = simplify(e.value)
-#         E2d, v2 = simplify(subst(e.body, e.var, v1))
-#         return composeContexts(E1d, E2d), v2
-#     elif(isinstance(e, Index) and isinstance(e.array, View)):
-#         subst_result = subst(e.array.body, e.array.var, e.index)
-#         return simplify(subst_result)
-#     elif(isinstance(e, Fst)):
-#         return simplify(e.pair.left)
-#     elif(isinstance(e, Snd)):
-#         return simplify(e.pair.right)
-#     elif(isinstance(e, For)):
-#         pass
+    if(isValue(e)):
+        return Hole(), e
+    elif(isinstance(e, Application)):
+        subst_result = subst(e.func.body, e.func.var, e.arg)
+        return simplify(subst_result)
+    elif(isinstance(e, Let)):
+        E1d, v1 = simplify(e.value)
+        E2d, v2 = simplify(subst(e.body, e.var, v1))
+        return composeContexts(E1d, E2d), v2
+    elif(isinstance(e, Index) and isinstance(e.array, View)):
+        subst_result = subst(e.array.body, e.array.var, e.index)
+        return simplify(subst_result)
+    elif(isinstance(e, Fst)):
+        return simplify(e.pair.left)
+    elif(isinstance(e, Snd)):
+        return simplify(e.pair.right)
+    
+    if(isinstance(e, For)):
+        Ed, v = simplify(e.body)
+        x1_n = bindingList(Ed, v)
+
+        # TODO Unclear about what free(x1,...,xn) is
+        #          x1, ..., xn should be free variables in v 
+        #            bounded by Ed
+        if(e.var not in x1_n):
+            y = get_fresh_var()
+            x = e.var
+            tau = e.var_type
+            x1_n_tuple = createTuple(x1_n)
+            Ed_with_subst = applyContext(Ed, x1_n_tuple)
+            return LetContext(y, UnspecifiedType(), For(x, Ed_with_subst, tau), Hole()), \
+                    View(x, Let(x1_n_tuple, Index(y, x), v, tau))
 
 
-#     type_infer = TypeInfer()
-#     tau_d = type_infer.infer(e)
-#     x = type_infer.fresh_typevar()
-#     return LetContext(x, tau_d, e, Hole()), x
+    if(isinstance(e, runAccum)):
+        Ed, v1 = simplify(e.update_fun.body)
+        x1_n = bindingList(Ed, v1, extraBinding=(e.update_fun.var, e.update_fun.param_type))
+        s = get_fresh_var()
+        x1_n_tuple = createTuple(x1_n)
+
+        return LetContext(Pair(x1_n_tuple, s), UnspecifiedType(), \
+                          runAccum(Function(e.update_fun.var, applyContext(Ed, x1_n_tuple), e.update_fun.param_type), e.init_val), Hole()), \
+                Pair(v1, s)
+
+    type_infer = TypeInfer()
+    tau_d = type_infer.infer(e)
+    x = type_infer.fresh_typevar()
+    return LetContext(x, tau_d, e, Hole()), x
---
+++ src/traversers.py
@@ -0,0 +1,295 @@
+from AST import *
+
+class ExprMutator:
+    """Base class for expression mutators that can transform the AST in various ways"""
+    def mutate(self, e : Expr | DexType) -> Expr | DexType:
+        """Dispatch to the appropriate method based on the expression type"""
+        if isinstance(e, Var):
+            return self.mutate_var(e)
+        elif isinstance(e, Application):
+            return self.mutate_application(e)
+        elif isinstance(e, Function):
+            return self.mutate_function(e)
+        elif isinstance(e, Fin):
+            return self.mutate_fin(e)
+        elif isinstance(e, View):
+            return self.mutate_view(e)
+        elif isinstance(e, Pair):
+            return self.mutate_pair(e)
+        elif isinstance(e, Let):
+            return self.mutate_let(e)
+        elif isinstance(e, Index):
+            return self.mutate_index(e)
+        elif isinstance(e, For):
+            return self.mutate_for(e)
+        elif isinstance(e, Fst):
+            return self.mutate_fst(e)
+        elif isinstance(e, Snd):
+            return self.mutate_snd(e)
+        elif isinstance(e, RefSlice):
+            return self.mutate_refslice(e)
+        elif isinstance(e, runAccum):
+            return self.mutate_runaccum(e)
+        elif isinstance(e, PlusEquals):
+            return self.mutate_plusequals(e)
+        elif isinstance(e, Add):
+            return self.mutate_add(e)
+        elif isinstance(e, Multiply):
+            return self.mutate_multiply(e)
+        elif isinstance(e, Float):
+            return self.mutate_float(e)
+        elif isinstance(e, Int):
+            return self.mutate_int(e)
+        elif isinstance(e, ArrayType):
+            return self.mutate_arraytype(e)
+        elif isinstance(e, FunctionType):
+            return self.mutate_functiontype(e)
+        elif isinstance(e, UnspecifiedType):
+            return self.mutate_unspecifiedtype(e)
+        elif isinstance(e, FloatType):
+            return self.mutate_floattype(e)
+        elif isinstance(e, IntType):
+            return self.mutate_inttype(e)
+        elif isinstance(e, UnitType):
+            return self.mutate_unittype(e)
+        elif isinstance(e, PairType):
+            return self.mutate_pairtype(e)
+        elif isinstance(e, RefType):
+            return self.mutate_reftype(e)
+        
+        assert False, "Expression type {} not handled".format(type(e))
+
+    # Default implementation for each type - identity transformation
+    def mutate_var(self, e: Var) -> Var:
+        return e
+    
+    def mutate_application(self, e: Application) -> Application:
+        return Application(self.mutate(e.func), self.mutate(e.arg))
+    
+    def mutate_function(self, e: Function) -> Function:
+        return Function(e.var, self.mutate(e.body), self.mutate(e.param_type))
+    
+    def mutate_fin(self, e: Fin) -> Fin:
+        return Fin(self.mutate(e.end))
+    
+    def mutate_view(self, e: View) -> View:
+        return View(self.mutate(e.var), self.mutate(e.body), self.mutate(e.var_type))
+    
+    def mutate_pair(self, e: Pair) -> Pair:
+        return Pair(self.mutate(e.left), self.mutate(e.right))
+    
+    def mutate_let(self, e: Let) -> Let:
+        return Let(e.var, self.mutate(e.value), self.mutate(e.body), self.mutate(e.var_type))
+    
+    def mutate_index(self, e: Index) -> Index:
+        return Index(self.mutate(e.array), self.mutate(e.index))
+    
+    def mutate_for(self, e: For) -> For:
+        return For(e.var, self.mutate(e.body), self.mutate(e.var_type))
+    
+    def mutate_fst(self, e: Fst) -> Fst:
+        return Fst(self.mutate(e.pair))
+    
+    def mutate_snd(self, e: Snd) -> Snd:
+        return Snd(self.mutate(e.pair))
+    
+    def mutate_refslice(self, e: RefSlice) -> RefSlice:
+        return RefSlice(self.mutate(e.ref), self.mutate(e.index))
+    
+    def mutate_runaccum(self, e: runAccum) -> runAccum:
+        return runAccum(self.mutate(e.update_fun))
+    
+    def mutate_plusequals(self, e: PlusEquals) -> PlusEquals:
+        return PlusEquals(self.mutate(e.dest), self.mutate(e.src))
+    
+    def mutate_add(self, e: Add) -> Add:
+        return Add(self.mutate(e.left), self.mutate(e.right))
+    
+    def mutate_multiply(self, e: Multiply) -> Multiply:
+        return Multiply(self.mutate(e.left), self.mutate(e.right))
+    
+    def mutate_arraytype(self, e: ArrayType) -> ArrayType:
+        return ArrayType(self.mutate(e.index_set), self.mutate(e.elmt_type))
+    
+    def mutate_functiontype(self, e: FunctionType) -> FunctionType:
+        return FunctionType(self.mutate(e.tau1), self.mutate(e.tau2), self.mutate(e.effect))
+    
+    def mutate_float(self, e: Float) -> Float:
+        return e
+     
+    def mutate_int(self, e: Int) -> Int:
+        return e
+    
+    def mutate_unspecifiedtype(self, e : UnspecifiedType) -> UnspecifiedType:
+        return e
+    
+    def mutate_floattype(self, e : FloatType) -> FloatType:
+        return e 
+    
+    def mutate_inttype(self, e: IntType) -> IntType:
+        return e
+    
+    def mutate_unittype(self, e: UnitType) -> UnitType:
+        return e
+    
+    def mutate_pairtype(self, e: PairType) -> PairType:
+        return PairType(self.mutate(e.tau1), self.mutate(e.tau2))
+    
+    def mutate_reftype(self, e: RefType) -> RefType:
+        return RefType(self.mutate(e.tau1), self.mutate(e.tau2))
+
+class ExprVisitor:
+    """Base class for expression visitors that can traverse the AST without transforming it"""
+    def visit(self, e: Expr | DexType) -> None:
+        """Dispatch to the appropriate method based on the expression type"""
+        if isinstance(e, Var):
+            self.visit_var(e)
+        elif isinstance(e, Application):
+            self.visit_application(e)
+        elif isinstance(e, Function):
+            self.visit_function(e)
+        elif isinstance(e, Fin):
+            self.visit_fin(e)
+        elif isinstance(e, View):
+            self.visit_view(e)
+        elif isinstance(e, Pair):
+            self.visit_pair(e)
+        elif isinstance(e, Let):
+            self.visit_let(e)
+        elif isinstance(e, Index):
+            self.visit_index(e)
+        elif isinstance(e, For):
+            self.visit_for(e)
+        elif isinstance(e, Fst):
+            self.visit_fst(e)
+        elif isinstance(e, Snd):
+            self.visit_snd(e)
+        elif isinstance(e, RefSlice):
+            self.visit_refslice(e)
+        elif isinstance(e, runAccum):
+            self.visit_runaccum(e)
+        elif isinstance(e, PlusEquals):
+            self.visit_plusequals(e)
+        elif isinstance(e, Add):
+            self.visit_add(e)
+        elif isinstance(e, Multiply):
+            self.visit_multiply(e)
+        elif isinstance(e, Float):
+            self.visit_float(e)
+        elif isinstance(e, Int):
+            self.visit_int(e)
+        elif isinstance(e, ArrayType):
+            self.visit_arraytype(e)
+        elif isinstance(e, FunctionType):
+            self.visit_functiontype(e)
+        elif isinstance(e, UnspecifiedType):
+            self.visit_unspecifiedtype(e)
+        elif isinstance(e, FloatType):
+            self.visit_floattype(e)
+        elif isinstance(e, IntType):
+            self.visit_inttype(e)
+        elif isinstance(e, UnitType):
+            self.visit_unittype(e)
+        elif isinstance(e, PairType):
+            self.visit_pairtype(e)
+        elif isinstance(e, RefType):
+            self.visit_reftype(e)
+        else:
+            assert False, f"Expression type {type(e)} not handled"
+
+    # Default implementation for each type - does nothing
+    def visit_var(self, e: Var) -> None:
+        pass
+    
+    def visit_application(self, e: Application) -> None:
+        self.visit(e.func)
+        self.visit(e.arg)
+    
+    def visit_function(self, e: Function) -> None:
+        self.visit(e.body)
+        self.visit(e.param_type)
+    
+    def visit_fin(self, e: Fin) -> None:
+        self.visit(e.end)
+    
+    def visit_view(self, e: View) -> None:
+        self.visit(e.body)
+        self.visit(e.var_type)
+    
+    def visit_pair(self, e: Pair) -> None:
+        self.visit(e.left)
+        self.visit(e.right)
+    
+    def visit_let(self, e: Let) -> None:
+        self.visit(e.value)
+        self.visit(e.body)
+        self.visit(e.var_type)
+    
+    def visit_index(self, e: Index) -> None:
+        self.visit(e.array)
+        self.visit(e.index)
+    
+    def visit_for(self, e: For) -> None:
+        self.visit(e.body)
+        self.visit(e.var_type)
+    
+    def visit_fst(self, e: Fst) -> None:
+        self.visit(e.pair)
+    
+    def visit_snd(self, e: Snd) -> None:
+        self.visit(e.pair)
+    
+    def visit_refslice(self, e: RefSlice) -> None:
+        self.visit(e.ref)
+        self.visit(e.index)
+    
+    def visit_runaccum(self, e: runAccum) -> None:
+        self.visit(e.update_fun)
+    
+    def visit_plusequals(self, e: PlusEquals) -> None:
+        self.visit(e.dest)
+        self.visit(e.src)
+    
+    def visit_add(self, e: Add) -> None:
+        self.visit(e.left)
+        self.visit(e.right)
+    
+    def visit_multiply(self, e: Multiply) -> None:
+        self.visit(e.left)
+        self.visit(e.right)
+    
+    def visit_float(self, e: Float) -> None:
+        pass
+     
+    def visit_int(self, e: Int) -> None:
+        pass
+    
+    def visit_arraytype(self, e: ArrayType) -> None:
+        self.visit(e.index_set)
+        self.visit(e.elmt_type)
+    
+    def visit_functiontype(self, e: FunctionType) -> None:
+        self.visit(e.tau1)
+        self.visit(e.tau2)
+        self.visit(e.effect)
+    
+    def visit_unspecifiedtype(self, e: UnspecifiedType) -> None:
+        pass
+    
+    def visit_floattype(self, e: FloatType) -> None:
+        pass
+    
+    def visit_inttype(self, e: IntType) -> None:
+        pass
+    
+    def visit_unittype(self, e: UnitType) -> None:
+        pass
+    
+    def visit_pairtype(self, e: PairType) -> None:
+        self.visit(e.tau1)
+        self.visit(e.tau2)
+    
+    def visit_reftype(self, e: RefType) -> None:
+        self.visit(e.tau1)
+        self.visit(e.tau2)
+
